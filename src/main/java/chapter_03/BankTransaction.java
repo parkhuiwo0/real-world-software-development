61e4ce8f4ed9b571b3c7dfb7a9fa556a416c2358
parkhuiwo0 <huiwo0@naver.com>
2022-01-15 02:20:18 +0900

    도메인의 특정 속성을 가지고 필터링하는 로직을 추가하라

---
+++ src/main/java/chapter_03/BankTransaction.java
@@ -0,0 +1,55 @@
+package chapter_03;
+
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * 거래 내역 도메인 클래스.
+ */
+public class BankTransaction {
+  private final LocalDate date;
+  private final double amount;
+  private final String description;
+
+  BankTransaction(LocalDate date, double amount, String description) {
+    this.date = date;
+    this.amount = amount;
+    this.description = description;
+  }
+
+  LocalDate getDate() {
+    return date;
+  }
+
+  double getAmount() {
+    return amount;
+  }
+
+  String getDescription() {
+    return description;
+  }
+
+  @Override
+  public String toString() {
+    return String.format("[BankTransaction] date=%s, amount=%f, description=%s",
+        date.toString(), amount, description);
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    BankTransaction that = (BankTransaction) o;
+    return Double.compare(that.amount, amount) == 0 &&
+        Objects.equals(date, that.date) &&
+        Objects.equals(description, that.description);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(date, amount, description);
+  }
+}
